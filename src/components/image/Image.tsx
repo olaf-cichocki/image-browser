4733e119b5a22bd3f06bf4723d45d141ea38fe3e
Viking <olaf.cichocki@gmail.com>
2019-06-03 23:42:16 +0200

    feat(Add lazy loading to image): Add svg placeholder showing up when image is still loading
    The image is hidden and positioned directly in the same place. Placeholder disappears after loading
    is finished. It has the same size so swap should look natural

--- src/components/image/Image.tsx
+++ src/components/image/Image.tsx
@@ -1,15 +1,70 @@
 import React from 'react';
 import css from './Image.module.scss';
+import classNames from 'classnames';
 
 interface ImageProps {
   src: string;
+  placeholderColor?: string;
+  width: number;
+  height: number;
 }
 
-class Image extends React.Component<ImageProps> {
-  render() {
-    const { src } = this.props;
+interface ImageState {
+  loaded: boolean;
+}
 
-    return <img src={src} />;
+class Image extends React.Component<ImageProps, ImageState> {
+  state = {
+    loaded: false
+  };
+
+  image = React.createRef<HTMLImageElement>();
+
+  componentDidUpdate(prevProps: ImageProps) {
+    const isSameImage = this.props.src === prevProps.src;
+    const isAlreadyLoaded = this.image.current!.complete;
+
+    if (isSameImage || (isAlreadyLoaded && this.state.loaded === true)) {
+      return;
+    }
+
+    if (isAlreadyLoaded && this.state.loaded === false) {
+      this.setState({ loaded: true });
+      return;
+    }
+
+    this.setState({ loaded: false });
+  }
+
+  private setLoaded = () => {
+    this.setState({ loaded: true });
+  };
+
+  render() {
+    const { src, width, height, placeholderColor = 'gray' } = this.props;
+    const { loaded } = this.state;
+
+    return (
+      <figure className={css.imageContainer}>
+        {!loaded && (
+          <svg
+            className={css.image}
+            width={width}
+            viewBox={`0 0 ${width} ${height}`}
+          >
+            <rect width={width} height={height} fill={placeholderColor} />
+          </svg>
+        )}
+        <img
+          ref={this.image}
+          className={classNames(css.image, {
+            [css.isLoading]: !loaded
+          })}
+          onLoad={this.setLoaded}
+          src={src}
+        />
+      </figure>
+    );
   }
 }
 
--- src/routes/Main.tsx
+++ src/routes/Main.tsx
@@ -6,7 +6,11 @@ const MainRoute: React.FC = () => (
     <header className="App-header">
       Main
       <section>
-        <Image src="https://via.placeholder.com/350x150" />
+        <Image
+          src="https://res.cloudinary.com/cyber-viking/image/upload/v1559591234/Krajobrazy/winter_scouts_by_dream_monger-d4eb12r_wr83ui.jpg"
+          width={1024}
+          height={559}
+        />
       </section>
     </header>
   </div>
