0e987192e294c72853044500469c6d63cea9f2a6
Viking <olaf.cichocki@gmail.com>
2019-06-03 23:53:57 +0200

    feat(Add error handling): Show red placeholder when there is error

--- src/components/image/Image.tsx
+++ src/components/image/Image.tsx
@@ -1,28 +1,40 @@
 import React from 'react';
 import css from './Image.module.scss';
 import classNames from 'classnames';
+import Placeholder from './Placeholder';
 
 interface ImageProps {
   src: string;
-  placeholderColor?: string;
   width: number;
   height: number;
+  placeholderColor?: string;
 }
 
 interface ImageState {
   loaded: boolean;
+  error: boolean;
 }
 
 class Image extends React.Component<ImageProps, ImageState> {
-  state = {
-    loaded: false
+  state: ImageState = {
+    loaded: false,
+    error: false
   };
 
   image = React.createRef<HTMLImageElement>();
 
+  static getDerivedStateFromError() {
+    return { error: true };
+  }
+
   componentDidUpdate(prevProps: ImageProps) {
     const isSameImage = this.props.src === prevProps.src;
     const isAlreadyLoaded = this.image.current!.complete;
+
+    if (this.state.error && !isSameImage) {
+      this.setState({ error: false });
+      return;
+    }
 
     if (isSameImage || (isAlreadyLoaded && this.state.loaded === true)) {
       return;
@@ -42,18 +54,24 @@ class Image extends React.Component<ImageProps, ImageState> {
 
   render() {
     const { src, width, height, placeholderColor = 'gray' } = this.props;
-    const { loaded } = this.state;
+    const { loaded, error } = this.state;
+
+    if (error) {
+      return (
+        <figure className={css.imageContainer}>
+          <Placeholder width={width} height={height} placeholderColor="red" />
+        </figure>
+      );
+    }
 
     return (
       <figure className={css.imageContainer}>
         {!loaded && (
-          <svg
-            className={css.image}
+          <Placeholder
             width={width}
-            viewBox={`0 0 ${width} ${height}`}
-          >
-            <rect width={width} height={height} fill={placeholderColor} />
-          </svg>
+            height={height}
+            placeholderColor={placeholderColor}
+          />
         )}
         <img
           ref={this.image}
---
+++ src/components/image/Placeholder.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import css from './Image.module.scss';
+
+interface ImageProps {
+  width: number;
+  height: number;
+  placeholderColor?: string;
+}
+
+const Placeholder: React.FC<ImageProps> = ({
+  width,
+  height,
+  placeholderColor = 'gray'
+}) => (
+  <svg className={css.image} width={width} viewBox={`0 0 ${width} ${height}`}>
+    <rect width={width} height={height} fill={placeholderColor} />
+  </svg>
+);
+
+export default Placeholder;
